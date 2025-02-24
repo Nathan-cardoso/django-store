1e10817d329e538d19f0b32cb88af99bf9c8e41f
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-02-24 19:58:55 -0300

    feat: add models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,102 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    title = models.CharField(
+        max_length=255
+    )
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+
+    inventory = models.IntegerField()
+
+    last_update = models.DateTimeField(auto_now=True)
+
+
+
+class Customer(models.Model):
+
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOISE = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+
+    first_name = models.CharField(
+        max_length=255
+    )
+
+    last_name = models.CharField(
+        max_length=255
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    phone = models.CharField(
+        max_length=15
+    )
+
+    birth_date = models.DateField(
+        null=True
+    )
+
+    membership = models.CharField(
+        max_length=1,
+        choices=MEMBERSHIP_CHOISE,
+        default=MEMBERSHIP_BRONZE
+    )
+
+
+class Adress(models.Model):
+
+    street = models.CharField(
+        max_length=255
+
+    )
+
+    city = models.CharField(
+        max_length=255
+    )
+
+    customer = models.OneToOneField(
+        Customer,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+
+
+
+class Order(models.Model):
+
+    PAYMENTSTATUS_PENDING = 'P'
+    PAYMENTSTATUS_COMPLETED = 'C'
+    PAYMENTSTATUS_FALIDED= 'F'
+
+    PAYMENTSTATUS_CHOISE = [
+        ('P', 'Pending'),
+        ('C', 'Completed'),
+        ('F', 'Faliled')
+    ]
+
+    placed_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    payment_status = models.CharField(
+        max_length=1,
+        choices=PAYMENTSTATUS_CHOISE,
+        default=PAYMENTSTATUS_PENDING
+    )
+
+
